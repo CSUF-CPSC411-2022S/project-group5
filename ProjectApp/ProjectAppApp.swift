8b48be1509d334d66b4923d2a0e39ea21f023a1f
ZachHofmeister <zachhof@me.com>
2022-02-23 10:39:53 -0800

    Initial Commit

---
+++ ProjectApp/ProjectAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectAppApp.swift
+//  ProjectApp
+//
+//  Created by Zach Hofmeister on 2/23/22.
+//
+
+import SwiftUI
+
+@main
+struct ProjectAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
