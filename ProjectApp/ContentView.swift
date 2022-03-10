bef738086267b8da83c7a78ce513761c58976603
barrotbake <nguyenjacob411@gmail.com>
2022-03-09 23:30:35 -0800

    added first 3d model and view for model

--- ProjectApp/ContentView.swift
+++ ProjectApp/ContentView.swift
@@ -6,12 +6,34 @@
 //
 
 import SwiftUI
+import RealityKit
 
 struct ContentView: View {
     var body: some View {
         Text("Hello, world!")
             .padding()
+        ARViewContainer().edgesIgnoringSafeArea(.all)
     }
+}
+
+struct ARViewContainer: UIViewRepresentable {
+    
+    func makeUIView(context: Context) -> ARView {
+        
+        let arView = ARView(frame: .zero)
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+        
+        return arView
+        
+    }
+    
+    func updateUIView(_ uiView: ARView, context: Context) {}
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
