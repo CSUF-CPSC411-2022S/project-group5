0d6f767109d86e0abfeee57ec38967271c0d5a0f
ZachHofmeister <zachhof@me.com>
2022-05-09 15:21:00 -0700

    Added pathfinder stuff

---
+++ CSUFExplore/Pathfinder/PGridNode.swift
@@ -0,0 +1,40 @@
+//
+//  PGridNode.swift
+//  BFS-Showcase
+//
+//  Created by Mixalis Dobekidis on 24/1/21.
+//
+
+import Foundation
+
+class PGridNode {
+    var x:Int = 0
+    var y:Int = 0
+    var type:Cell!
+    var opened:Bool = false
+    var closed:Bool = false
+    var parent:PGridNode!
+    
+    init(x:Int, y:Int, type:Cell) {
+        self.x = x
+        self.y = y
+        self.type = type
+    }
+    
+    func getX() -> Int {
+        return self.x
+    }
+    
+    func getY() -> Int {
+        return self.y
+    }
+    
+    func getType() -> Cell {
+        return type ?? .Empty
+    }
+    
+    func getAsML() -> MazeLocation {
+        return MazeLocation(row: x, col: y)
+    }
+}
+
